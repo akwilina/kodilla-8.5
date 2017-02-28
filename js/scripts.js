89c55a591a7e354b68a84330bd5a1edbbb7ca1ce
delola\delola lokal <aleksandra.man@gmail.com>
2017-02-28 22:35:02 +0100

    deleted alerts, used "short if", added spaces between operators

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,8 @@
 var a = prompt("podaj wartość a (domyślna wartość to 1)", 1);
 var b = prompt("podaj wartość h (domyślna wartość to 2)", 2);
-var value = (a*a) + (2 * a * b) - (b*b) ;
+var value = (a * a) + (2 * a * b) - (b * b) ;
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + b + ' wynosi: ' + value);
-alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + b + ' wynosi: ' + value);
 
-if (value > 0) {
-    console.log("wynik dodatni");
-    alert("wynik dodatni");
-} else if (value == 0) {
-    console.log("wynik wynosi 0");
-    alert("wynik wynosi 0");
-} else {
-    console.log("wynik ujemny");
-    alert("wynik ujemny");
-}
+var triangleArea = value > 0 ? value == 0 ? 'wynik wynosi 0' : 'wynik ujemny' : 'wynik dodatni';
+console.log(triangleArea);
